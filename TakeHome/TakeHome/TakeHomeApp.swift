7f0a6f6057669b8db1547068e7c4c4c8d4a799c5
Nicolas Cobelo <>
2024-06-20 21:55:48 -0300

    Initial commit

---
+++ TakeHome/TakeHome/TakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeApp.swift
+//  TakeHome
+//
+//  Created by Nicolas Cobelo on 20/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
